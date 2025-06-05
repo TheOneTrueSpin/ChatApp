6d08c435c626eebc9fcfd657ac775f4aaf00cdd5
TheOneTrueSpin <lspinelli@thespincorp.com>
2025-06-05 12:25:55 +0000

    added dto to get messages

--- ChatApp/Features/ChatFeatures/Dtos/MessageResponseDto.cs
+++ ChatApp/Features/ChatFeatures/Dtos/MessageResponseDto.cs
@@ -2,9 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ChatApp.Database.Entities;
 
 namespace ChatApp.Features.ChatFeatures.Dtos;
 public class MessageResponseDto
 {
-    
+    public required Guid Id {get;set;}
+    public required DateTime SentOnUTC {get;set;}
+    public required Guid SenderId {get; set;}
+    public required string MessageContents {get; set;}
+    public required Guid ChatId {get;set;}
 }
--- ChatApp/Features/ChatFeatures/GetMessagesFeature.cs
+++ ChatApp/Features/ChatFeatures/GetMessagesFeature.cs
@@ -16,25 +16,46 @@ using ChatApp.Shared.Services.ChatServices;
 using ChatApp.Database.Entities;
 using ChatApp.Shared.Services.Auth;
 using FirebaseAdmin.Auth;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
 
 namespace ChatApp.Features.ChatFeatures;
 
 public static class GetMessageFeature
 {
+    public class GetMessageResonse
+    {
+        public List<MessageResponseDto> MessageResponseDtos { get; set; } = new List<MessageResponseDto>();
+    }
     public class Handler : IScoped
     {
         private readonly IChatService _chatService;
         private readonly IAuthService _authService;
+
         public Handler(IChatService chatService, IAuthService authService)
         {
             _chatService = chatService;
             _authService = authService;
         }
-        public async Task<List<Message>> Handle(Guid chatId, CancellationToken cancellationToken)
+        public async Task<GetMessageResonse> Handle(Guid chatId, CancellationToken cancellationToken)
         {
             Guid userId = _authService.GetUserId();
             List<Message> messages = await _chatService.GetMessages(chatId, userId);
-            return messages;
+            GetMessageResonse getMessageResonse = new GetMessageResonse();
+            foreach (var message in messages)
+            {
+                MessageResponseDto currentMessage = new MessageResponseDto()
+                {
+                    Id = message.Id,
+                    SentOnUTC = message.SentOnUTC,
+                    SenderId = message.SenderId,
+                    MessageContents = message.MessageContents,
+                    ChatId = message.ChatId
+                    
+                };
+                getMessageResonse.MessageResponseDtos.Add(currentMessage);
+            }
+
+            return getMessageResonse;
         }
     }
 }
@@ -45,8 +66,8 @@ public class GetMessageEndpoint : ICarterModule
     {
         app.MapGet("api/ChatFeatures/Messages", async (Guid chatId, Handler handler, CancellationToken cancellationToken) =>
         {
-            List<Message> messages = await handler.Handle(chatId, cancellationToken);
-            return Results.Ok(messages);
+            GetMessageResonse response = await handler.Handle(chatId, cancellationToken);
+            return Results.Ok(response);
         })
         .WithTags("ChatFeatures")
         .WithMetadata(new AuthorizeAttribute { Roles = "User" })
